9fe4e7999397ed6bd3a27cd7250ce742bddb0df0
Adrien De Mey <adriendemey@gmail.com>
2015-04-06 16:11:40 +0200

    Header
    * Add image,
    * Set colors and font,
    * Create left triangle (flat design, you know...)
    * Also worked on nav bar.

--- pages/views/includes/header.php
+++ pages/views/includes/header.php
@@ -5,6 +5,18 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <title>{title}</title>
-    <link rel="stylesheet" href="<?= css_url('unsemantic-grid-responsive');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('unsemantic-grid-responsive');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('base');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('fonts_import');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('header');?>" />
 </head>
 <body>
+    <div class="grid-container">
+        <header class="grid-100">
+            <h1>Accueil</h1>
+        </header>
+        <nav class="grid-100">
+            <a href="#" class="grid-33">Accueil - À propos de moi...</a>
+            <a href="blog/" class="grid-33">Blog - Des Roses Et Des Aiguilles</a>
+            <a href="blog/" class="grid-33">Galerie</a>
+        </nav>
