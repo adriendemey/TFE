ab517baee14112f6c87308bd5154396564760c86
Adrien De Mey <adriendemey@gmail.com>
2015-04-18 13:19:29 +0200

    nav finished

--- pages/views/includes/header.php
+++ pages/views/includes/header.php
@@ -6,17 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <title>{title}</title>
     <link rel="stylesheet" href="<?php echo css_url('unsemantic-grid-responsive');?>" />
-    <link rel="stylesheet" href="<?php echo css_url('base');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('normalize');?>" />
     <link rel="stylesheet" href="<?php echo css_url('fonts_import');?>" />
     <link rel="stylesheet" href="<?php echo css_url('header');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('content');?>" />
+    <link rel="stylesheet" href="<?php echo css_url('footer');?>" />
 </head>
 <body>
     <div class="grid-container">
         <header class="grid-100">
             <h1>Accueil</h1>
         </header>
-        <nav class="grid-100">
+        <nav class="grid-100 grid-parent">
             <a href="#" class="grid-33">Accueil - À propos de moi...</a>
             <a href="blog/" class="grid-33">Blog - Des Roses Et Des Aiguilles</a>
-            <a href="blog/" class="grid-33">Galerie</a>
+            <a href="galerie/" class="grid-33">Galerie</a>
         </nav>
